3728fe45a498bf6a78946a267a8c5118618e8d16
Vitinho2 <vi.vitorsouza150@gmail.com>
2024-03-11 00:45:37 -0300

    criando um modelo de lista de usuario estatica

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+// Sidebar Funcionalidade
 let btn = document.querySelector('#btn')
 let sidebar = document.querySelector('.sidebar')
 
 btn.onclick = function() {
     sidebar.classList.toggle('active')
 }
+
+// API Random User
+const apiKey = 'LPL8-CTCY-7XX1-HS9E'
