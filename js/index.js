19e719953c77d3492aa849897be22fd200daf164
Vitinho2 <vi.vitorsouza150@gmail.com>
2024-03-12 11:11:02 -0300

    Exibindo apenas 1 usuario aleatorio, com consumo de API

--- js/index.js
+++ js/index.js
@@ -7,4 +7,27 @@ btn.onclick = function() {
 }
 
 // API Random User
-const apiKey = 'LPL8-CTCY-7XX1-HS9E'
+const img = document.getElementById("img")
+const loc = document.getElementById("address-user")
+const name = document.getElementById("name-user")
+const email = document.getElementById("email-user")
+const birthday = document.getElementById("birthday")
+const number = document.getElementById("number-user")
+
+async function users(){
+  const response = await fetch('https://randomuser.me/api/')
+  const result = await response.json()
+  console.log(result)
+  setUserInfo(result)
+}
+
+function setUserInfo(result){
+  img.src = result.results[0].picture.large
+  loc.textContent = result.results[0].location.city + ", " + result.results[0].location.country
+  name.textContent = result.results[0].name.first + " " + result.results[0].name.last
+  email.textContent = result.results[0].email
+  birthday.textContent = result.results[0].dob.date.split("T")[0]
+  number.textContent = result.results[0].phone
+}
+
+users()
